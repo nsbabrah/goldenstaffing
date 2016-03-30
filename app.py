7684562e4c3b81fdf1f9b000eb415c814d185902
nsbabrah <navjotbabrah27@gmail.com>
2016-03-30 18:50:02 -0400

    Flask-py-file

---
+++ app.py
@@ -0,0 +1,71 @@
+from flask import Flask, render_template, request
+import requests
+
+
+
+app = Flask(__name__)
+
+# Mailgun Base URL
+URL = 'https://api.mailgun.net/v3/goldenstaffing.ca/messages'
+MAILGUN_API_KEY = 'key-a6678793f067234695575a791ddffb75'
+
+
+@app.route('/')
+def index():
+	return render_template('bc.html')
+
+
+
+
+@app.route('/contact', methods=['POST'])
+def contact():
+
+	if request.method == 'POST':
+
+		params = request.form
+		print params
+		text = params['message']
+		phone = params['phone'] 
+		name = params['name']
+		email = params['email']
+		resume = request.files['resume']
+		text =  text  + make_footer(name,phone,email)
+		if resume:
+			send_mail(text,resume)
+		else:
+			send_mail(text)
+		return render_template('bc.html',message='Thank You for the Inquiry!')
+
+def send_mail(text,resume=None):
+		if resume:
+			files = [('attachment',resume)]
+		else:
+			files = []
+		return requests.post(URL,
+        auth = ("api", MAILGUN_API_KEY),
+         files=files,
+        data = {"from": "Golden Staffing Services <postmaster@goldenstaffing.ca>",
+               "to": ['goldenstaffingservices@gmail.com'],
+               "subject": 'Golden Staffing Inquiry',
+               "text": text
+               })
+
+
+def make_footer(name,number,email):
+	t = '\n\nRegards,\n'
+	t += name+ '\n'
+	t += number + '\n'
+	t += email + '\n'
+
+	return t
+
+@app.route('/contact', methods=['POST'])
+def redirect():
+	if request.method == 'POST':
+		if request.form('search' ,None) == "Like":
+			return redirect(url_for('bc.html'))
+		elif request.form('search',None) == "Dislike":
+			return redirect(url_for('bc.html'))
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=5000, debug=True)
